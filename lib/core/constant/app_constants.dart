0d2751100c71ed2e78ebadecf7861ed9b5b22ff6
mahmoud123asd <https://github.com/mahmoud123asd>
2026-07-31 23:22:36 +0300

    Initial commit: Upload Flutter project

---
+++ lib/core/constant/app_constants.dart
@@ -0,0 +1,23 @@
+class AppConstants {
+  AppConstants._();
+
+  // Collections
+  static const String usersCollection = 'users';
+  static const String postsCollection = 'posts';
+  static const String commentsCollection = 'comments';
+  static const String likesCollection = 'likes';
+
+  // Limits
+  static const int paginationLimit = 10;
+  static const int maxPostTitleLength = 100;
+  static const int maxPostContentLength = 2000;
+  static const int maxCommentLength = 500;
+
+  // Durations
+  static const Duration splashDuration = Duration(seconds: 3);
+  static const Duration animationDuration = Duration(milliseconds: 300);
+
+  // Design sizes
+  static const double designWidth = 390.0;
+  static const double designHeight = 884.0;
+}
