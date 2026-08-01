2b5d408e10ee4d8127b5a98aed8c6d907d6b261d
mahmoud123asd <https://github.com/mahmoud123asd>
2026-08-01 16:24:56 +0300

    Add login route and view to AppRouter

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:go_router/go_router.dart';
 
+import '../../features/auth/presentation/view/login_view.dart';
 import '../../features/splash/presentation/view/splash_view.dart';
 import 'routes.dart';
 
@@ -13,6 +14,11 @@ class AppRouter {
         path: Routes.splash,
         name: RouteNames.splash,
         builder: (context, state) => const SplashView(),
+      ),
+      GoRoute(
+        path: Routes.login,
+        name: RouteNames.login,
+        builder: (context, state) => const LoginView(),
       ),
     ],
   );
--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -2,10 +2,12 @@ class Routes {
   Routes._();
 
   static const String splash = '/';
+  static const String login = '/login';
 }
 
 class RouteNames {
   RouteNames._();
 
   static const String splash = 'splash';
+  static const String login = 'login';
 }
