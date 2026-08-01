f9b0174547f8ce24f428f3bf0e3f04e4be3e0aab
mahmoud123asd <https://github.com/mahmoud123asd>
2026-08-01 16:24:19 +0300

    Add SVG icons for authentication, eye, lock, and mail

--- lib/core/constant/app_assets.dart
+++ lib/core/constant/app_assets.dart
@@ -1,5 +1,9 @@
 class AppAssets {
   AppAssets._();
 
+  static const String authLogo = 'assets/icons/auth_logo.svg';
   static const String connectHubLogo = 'assets/icons/connect_hub_logo.svg';
+  static const String eye = 'assets/icons/eye.svg';
+  static const String lock = 'assets/icons/lock.svg';
+  static const String mail = 'assets/icons/mail.svg';
 }
