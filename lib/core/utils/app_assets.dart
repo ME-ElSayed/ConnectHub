2c251db01513f7eda88b67c11061d2be393d72df
mahmoud123asd <https://github.com/mahmoud123asd>
2026-08-02 03:29:38 +0300

    Add avatar icon to AppAssets: include new avatar.png asset for user profile representation

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -1,9 +1,11 @@
 class AppAssets {
   AppAssets._();
-
-  static const String authLogo = 'assets/icons/auth_logo.svg';
-  static const String connectHubLogo = 'assets/icons/connect_hub_logo.svg';
-  static const String eye = 'assets/icons/eye.svg';
-  static const String lock = 'assets/icons/lock.svg';
-  static const String mail = 'assets/icons/mail.svg';
+  static const String root = 'assets/icons';
+  static const String authLogo = '$root/auth_logo.svg';
+  static const String connectHubLogo = '$root/connect_hub_logo.svg';
+  static const String eye = '$root/eye.svg';
+  static const String lock = '$root/lock.svg';
+  static const String mail = '$root/mail.svg';
+  static const String profile = '$root/profile.svg';
+  static const String avatar = '$root/avatar.png';
 }
